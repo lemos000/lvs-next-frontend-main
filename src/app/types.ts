0778cb307480bfc770c45c49b21c99bb67e846df
laviniashpark <laviniasoo03@gmail.com>
2024-11-04 20:35:20 -0300

    Criação das tipagens

---
+++ src/app/types.ts
@@ -0,0 +1,15 @@
+export type TipoUsuario = {
+    nome: string,
+    email: string,
+    senha: string
+}
+
+export type TipoOficina = {
+
+    nome: string,
+    endereco: string,
+    contato: string,
+    status:string 
+
+
+}
